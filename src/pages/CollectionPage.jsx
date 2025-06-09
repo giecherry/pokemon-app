e24650c0e4a7e78d16a534b90a75a0a44eaa1a3c
Angie C <a.cajusol98@gmail.com>
2025-06-10 00:24:42 +0200

    reboot backend setup

--- src/pages/CollectionPage.jsx
+++ src/pages/CollectionPage.jsx
@@ -1,18 +1,10 @@
-import { SignedIn, SignedOut, RedirectToSignIn, UserButton } from '@clerk/clerk-react';
 import HomeBtn from '../components/HomeBtn';
 
 function CollectionPage() {
     return (
         <>
-            <SignedIn>
                 <h1>My Collection</h1>
-                <UserButton />
                 {/* Your collection component */}
-            </SignedIn>
-
-            <SignedOut>
-                <RedirectToSignIn />
-            </SignedOut>
             <HomeBtn />
         </>
     );
